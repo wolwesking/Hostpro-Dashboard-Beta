79d390edeefd078804c75d4b202c56438e3458a0
Wolwesking <takacspatrik993@gmail.com>
2024-09-10 19:11:33 +0200

    Instructions

--- frontend/src/lib/server/actiavte/satisfactory.ts
+++ frontend/src/lib/server/actiavte/satisfactory.ts
@@ -2,8 +2,9 @@ import pb from "$lib/Pocketbase";
 
 export async function activateSatisfactoryBasic(data: any) {
     try {
-        const record = await pb.collection('Subsciptions').create(data);
-
+        await pb.collection('Subsciptions').create(data);
+        // TODO make this so only admins can do it!
+        // TODO pterodactyl generate user, and generate server
     } catch (error) {
         console.error(error)
     }
